6371be716188a2edf0e86c763a406da6538c5c41
jakouk <jakoriaty@gmail.com>
2018-06-20 23:01:31 +0900

    Add removeChars

--- Chapter6 Array & String/Chapter6 Array & String/main.swift
+++ Chapter6 Array & String/Chapter6 Array & String/main.swift
@@ -12,6 +12,7 @@ func firstNonRepeated(str: String) -> Character? {
   var charHash = [Character: Int]()
   let stringArray = Array(str)
   
+  // n
   for char in stringArray {
     if charHash[char] == nil {
       charHash[char] = 1
@@ -20,14 +21,42 @@ func firstNonRepeated(str: String) -> Character? {
     }
   }
   
+  // n
   for char in stringArray {
     if charHash[char] == 1 {
       return char
     }
   }
   
+  // 2n = O(n)
+  
   return nil
 }
 
 print("firstNonRepeated = \(firstNonRepeated(str: "HelloH")!)")
 
+func removeChars(str: String, remove: String) -> String {
+  var strArray = Array(str)
+  var removeArray = Array(remove)
+  var newString = ""
+  
+  var flags = [Character: Bool]()
+  
+  // r
+  for i in 0..<removeArray.count {
+    flags[removeArray[i]] = true
+  }
+  
+  // m
+  for i in 0..<strArray.count {
+    if flags[strArray[i]] == nil {
+      newString.append(strArray[i])
+    }
+  }
+  
+  // r + m -> 2n -> O(n)
+  
+  return newString
+}
+
+print("removerChars = \(removeChars(str: "Battle of the Vowels: Hawaii vs Grozny", remove: "aeiou"))")
